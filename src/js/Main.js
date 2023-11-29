3e3ea7901611950f0a39f97f7dbfddf237a1c2b8
spookybeetle <krw5520@psu.edu>
2023-11-29 12:38:25 -0500

    add main.js

---
+++ src/js/Main.js
@@ -0,0 +1,15 @@
+import '../css/Main.css';
+
+
+export default function Main() {
+return(
+
+<ul class="shadow-button-set">
+  <li><button>About</button></li>
+  <li><button>Art</button></li>
+  <li><button>Games</button></li>
+  <li><button>Boba Dragons</button></li>
+  <li><button>Contact</button></li>
+</ul>
+);
+}
