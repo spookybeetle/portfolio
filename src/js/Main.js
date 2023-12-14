2bf9053e35d250eebd7bd502a1ec374a77f7d575
spookybeetle <krw5520@psu.edu>
2023-12-13 19:55:00 -0500

    Add links to extending pages

--- src/js/Main.js
+++ src/js/Main.js
@@ -6,11 +6,11 @@ export default function Main() {
 return(
 
 <ul class="shadow-button-set">
-  <li><button>About</button></li>
-  <li><button>Art</button></li>
-  <li><button>Games</button></li>
-  <li><button>Boba Dragons</button></li>
-  <li><button>Contact</button></li>
+  <li><a href="https://spookybeetle.com/home.php"><button>About</button></a></li>
+  <li><a href="https://spookybeetle.com/art.php"><button>Art</button></a></li>
+  <li><a href="https://spookybeetle.com/home.php"><button>Games</button></a></li>
+  <li><a href="https://www.bobadragons.com/"><button>Boba Dragons</button></a></li>
+  <li><a href="https://spookybeetle.com/contact.php"><button>Contact</button></a></li>
 </ul>
 );
 }
